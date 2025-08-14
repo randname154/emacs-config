f7c480b49c333adf733e38fef2ebd017580fc0e5
YangWencong <yangwencong@pwrd.com>
2025-08-14 20:09:58 +0800

    remove packages

--- lisp/custom.el
+++ lisp/custom.el
@@ -6,9 +6,9 @@
  ;; If there is more than one, they won't work right.
  '(package-selected-packages
    '(company-box company-prescient company-quickhelp consult embark
-		 embark-consult embark-vc gptel json-mode magit
-		 marginalia nerd-icons org-download org-roam-ui
-		 ox-pandoc transient vertico-prescient zig-ts-mode)))
+		 embark-consult json-mode marginalia nerd-icons
+		 org-download org-roam-ui ox-pandoc transient
+		 vertico-prescient zig-ts-mode)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
